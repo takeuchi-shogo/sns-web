4836dd657aaeb9cb747ff0264e7d2c42c580333b
ShogoTakeuchi <smth.1.23@icloud.com>
2023-05-06 20:41:18 +0900

    install routify

--- app/main.js
+++ app/main.js
@@ -1,4 +1,10 @@
-import style from '/sass/style.scss' //import を使って style.scss(sass)を読み込む
+import '/sass/style.scss' //import を使って style.scss(sass)を読み込む
 import 'bootstrap'
 
-import routes from '/src/infrastructure/Routing.js'
+import HMR from '@roxi/routify/hmr'
+import App from './App.svelte'
+
+
+const app = HMR(App, { target: document.body }, "routify-app")
+
+export default app
